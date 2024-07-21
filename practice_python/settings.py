c3e11228fa67a77d8a6b7d28a991489054b5d3ca
홍태호 <malamute10@xinapse.ai>
2024-07-21 16:31:27 +0900

    chore: add DRF

--- practice_python/settings.py
+++ practice_python/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
+
+TIME_ZONE = 'Asia/Seoul'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
