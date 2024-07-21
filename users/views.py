55e99721dc5f5ad844f4118fed590c99a60b21c9
홍태호 <malamute10@xinapse.ai>
2024-07-21 19:15:17 +0900

    feat : user 저장기능 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
 
-# Create your views here.
+from users.secializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
